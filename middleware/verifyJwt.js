04fe007df2d89a7cc72e2d29dcebed9657ca151d
Folorunso praise Oluwapelumi <106927972+praisezee@users.noreply.github.com>
2023-05-10 03:14:11 +0100

    Update verifyJwt.js

--- middleware/verifyJwt.js
+++ middleware/verifyJwt.js
@@ -13,7 +13,7 @@ const verifyJwt = ( req, res, next ) =>
             ( err, decoded ) =>
             {
                   if ( err ) return res.sendStatus( 403 );// invalid token
-                  res.user = decoded.email || decoded.phoneNumber;
+                  res.user = decoded.email;
                   next()
             }
       )
